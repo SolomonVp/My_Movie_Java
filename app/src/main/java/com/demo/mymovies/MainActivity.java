c0c2da7f67caeb4ef84f60ec83d6c446f8297a71
SolomonVP <Lum7023@yandex.ru>
2023-08-23 22:59:06 +0300

    init

---
+++ app/src/main/java/com/demo/mymovies/MainActivity.java
@@ -0,0 +1,20 @@
+package com.demo.mymovies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.demo.mymovies.utils.NetworkUtils;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String url = NetworkUtils.buildURL(NetworkUtils.POPULARITY, 1).toString();
+        Log.i("MyResult", url);
+    }
+}
